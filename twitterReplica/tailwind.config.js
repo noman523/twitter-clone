fce534c95fcb5f1da033c8442a5fd94acbd9bdd3
noman523 <37392852+noman523@users.noreply.github.com>
2021-09-12 02:13:16 +0600

    .

---
+++ twitterReplica/tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      blue: {
+        level4: '#1A8CD8',
+        level3: '#1B95E0',
+        level2: '#1D9BF0',
+        level1: 'rgba(29, 155, 240, 0.1);',
+      },
+      gray: {
+        level6: '#0F1419',
+        level5: '#536471',
+        level4: '#E7E7E8',
+        level3: '#EFF3F4',
+        level2: 'rgba(0, 0, 0, 0.03)',
+        level1: '#F7F9F9',
+      },
+      green: {
+        default: '#22C55E',
+      },
+      white: {
+        default: '#ffffff',
+      },
+      
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
